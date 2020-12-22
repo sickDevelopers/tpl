85c0ddd033841b6ec9b8b82f6445f4c33551016c
Oscar Chinellato <oscar.chinellato@sickevelopers.com>
2020-12-22 15:09:26 +0100

    catalog

---
+++ catalog.js
@@ -0,0 +1,29 @@
+const catalog = `#000000,Black,Black,"Bricks Special",614126,6141,"€ 0.04",
+#cf4ba0,Purple,Br. Purple,"Bricks Special",6195315,6141,"€ 0.04",TROVATO EXTRA
+#cf4ba0,,Br.Green,"Bricks Special",6109808,6141,"€ 0.04",
+#e68432,,Br.Orange,"Bricks Special",4157103,6141,"€ 0.04",
+#d73232,Red,Br.Red,"Bricks Special",614121,6141,"€ 0.04",
+#7a3d71,Purple,Br.Red-Viol.,"Bricks Special",6248970,6141,"€ 0.06",TROVATO EXTRA
+#f5df5a,Yellow,Br.Yel,"Bricks Special",614124,6141,"€ 0.04",
+#b2d25f,,Br.Yel-Green,"Bricks Special",4183133,6141,"€ 0.04",
+#c1ad7c,,Brick-Yel,"Bricks Special",4161734,6141,"€ 0.04",
+#ffff8c,Yellow,Cool Yel.,"Bricks Special",6319958,6141,"€ 0.04",TROVATO EXTRA
+#312415,,Dk. Brown,"Bricks Special",6315780,6141,"€ 0.04",TROVATO EXTRA
+#5a5a5a,Grey,Dk. St. Grey,"Bricks Special",4210633,6141,"€ 0.04",
+#4ba45b,,Dk.Green,"Bricks Special",4569058,6141,"€ 0.04",
+#824830,,Dk.Ora,"Bricks Special",6315782,6141,"€ 0.04",TROVATO EXTRA
+#284e96,Blue,Earth Blue,"Bricks Special",6021623,6141,"€ 0.04",TROVATO EXTRA
+#efc1f0,Purple,Lgh. Purple,"Bricks Special",4517996,6141,"€ 0.04",
+#6659ac,,M. Lilac,"Bricks Special",4566522,6141,"€ 0.04",
+#6f5238,,M. Nougat,"Bricks Special",6167641,6141,"€ 0.04",TROVATO EXTRA
+#9b9b9b,Grey,Med. St-Grey,"Bricks Special",4211525,6141,"€ 0.04",
+#64a1cd,Blue,Medium Azur,"Bricks Special",6102986,6141,"€ 0.04",TROVATO EXTRA
+#572529,Red,New Dark Red,"Bricks Special",6314247,6141,"€ 0.04",TROVATO EXTRA
+#55372f,,Red. Brown,"Bricks Special",4216581,6141,"€ 0.04",
+#6d8690,Blue,Sand Blue,"Bricks Special",6315783,6141,"€ 0.04",TROVATO EXTRA
+#827661,,Sand Yellow,"Bricks Special",6315781,6141,"€ 0.04",TROVATO EXTRA
+#6e6e6e,,Silver Met.,"Bricks Special",4633691,6141,"€ 0.04",
+#c8da8e,,Spr. Yell. Gree,"Bricks Special",6057898,6141,"€ 0.04",TROVATO EXTRA
+#3c3c3c,Grey,Titan. Metal.,"Bricks Special",6038201,6141,"€ 0.04",TROVATO EXTRA
+#a07c3a,,W.Gold,"Bricks Special",4523159,6141,"€ 0.04",TROVATO EXTRA
+#ffffff,White,White,"Bricks Special",614101,6141,"€ 0.04",`
